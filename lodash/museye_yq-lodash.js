9f985c807cc28b8858cc7ce121322427690ef957
Your Name <you@example.com>
2021-07-12 19:19:43 +0800

    main

--- lodash/museye_yq-lodash.js
+++ lodash/museye_yq-lodash.js
@@ -31,7 +31,77 @@ var museye_yq = function () {
   }
 
 
-  function concat() {
+  function differencr(array, values) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      if (!values.includes(array[i])) {
+        result.push(array[i])
+      }
+    }
+    return result
+  }
+
+
+  function differenceBy(array, values, iteratee) {
+    for (var i = 0; i < array.length; i++) {
+      array.splice(i, 1, iteratee(array[i])
+    }
+    for (var i = 0; i < values.length; i++) {
+      values.splice(i, 1, iteratee(array[i])
+    }
+    var result = []
+    result = differencr(array, values)
+    return result
+  }
+
+
+  function differenceWith(array, values, comparator) {
 
   }
+
+
+  function drop(array, n) {
+    if (n == undefined) {
+      n = 1
+    }
+    array.splice(0, n)
+    return array
+  }
+
+
+  function dropRight(array, n) {
+    if (n == undefined) {
+      n = 1
+    }
+    if (n > array.length) {
+      return []
+    }
+    array.splice(array.length - n, n)
+    return array
+  }
+
+
+  function fill(array, value, start, end) {
+    if (start == undefined) {
+      start = 0
+    }
+    if (end == undefined) {
+      end = array.length
+    }
+    for (var i = start; i < end; i++) {
+      array[i] = value
+    }
+    return array
+  }
+
+
+  return {
+    chunk: chunk,
+    compact: compact,
+    differencr: differencr,
+    differenceBy: differenceBy,
+    drop: drop,
+    dropRight: dropRight,
+    fill: fill,
 }
+}()
